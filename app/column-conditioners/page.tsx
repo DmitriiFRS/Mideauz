d25a167458457f3b0a42c218cb8d0aefc6684407
Дмитрий <Dmitriifrs@gmail.com>
2023-10-07 09:24:05 +0500

    commit

--- app/column-conditioners/page.tsx
+++ app/column-conditioners/page.tsx
@@ -1,6 +1,19 @@
+import {mideaConditionersData} from '../Data/ColumnConditionersData';
+import {welkinConditionersData} from '../Data/ColumnConditionersData';
+import Conditioners from './Conditioners';
+
 function ColumnConditioners() {
+   const data = [{
+      title: 'Кондиционеры Midea',
+   },
+   {
+      title: 'Кондиционеры Welkin',
+   }]
    return (
-      <div className='flex-auto'></div>
+      <section className='flex-auto'>
+         <Conditioners title={data[0].title} data={mideaConditionersData}/>
+         <Conditioners title={data[1].title} data={welkinConditionersData}/>
+      </section>
    )
 }
 export default ColumnConditioners;
