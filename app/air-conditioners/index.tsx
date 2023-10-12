a71928a086f0daf95422277d9edad8c092c1572d
Дмитрий <Dmitriifrs@gmail.com>
2023-10-12 09:34:07 +0500

    update

--- app/air-conditioners/index.tsx
+++ app/air-conditioners/index.tsx
@@ -14,12 +14,12 @@ type dataType = {
       id: string
    }
 }
-
 type ConditionersProps = {
-   brands: Array<string>
-   data: Array<dataType>
+   brand: string
+   data: Array<dataType>,
+   imges: Array<any>
 }
-function ConditionersList({brands, data}: ConditionersProps) {
+function ConditionersList({brand, data, imges}: ConditionersProps) {
    useEffect(() => {
       console.log(data)
    }, [])
@@ -32,6 +32,33 @@ function ConditionersList({brands, data}: ConditionersProps) {
       setIsView(inView)
    }, [inView])
    return (
+   <div className="conditioners container">
+      <h2 className="text-center font-bold text-6xl text-slate-600">{brand}</h2>
+      <ul ref={ref} className="conditioners__list grid grid-cols-3 gap-12 mt-14">
+         {data.map((el, index) => {
+            return (
+               <Link key={index} href={`/air-conditioners/${el.main.id}`}>
+                  <li style={{transitionDelay: (index / 6) + 's'}} className={`conditioners__item ${isView ? 'conditioners__item__active' : ''} flex flex-col text-center items-center py-6`} key={index}>
+                     <div className='conditioners__img-container relative flex items-center justify-center z-0'>
+                        <Image className='conditioners__img' src={imges[index]} alt={el.main.name} fill={true} />
+                     </div>
+                     <div className='flex flex-col'>
+                        <p className='text-5xl font-medium relative z-10'>{el.main.name}</p>
+                        <span className='text-2xl mt-5 inline-block'>{el.main.subname}</span>
+                        <span className='text-xl mt-5 inline-block'>от 999.999 UZS</span>
+                     </div>
+                     <button className='conditioners__btn mt-10'>Выбрать параметры</button>
+                  </li>
+               </Link>
+            )
+         })}
+      </ul>
+   </div>
+   )
+}
+export default ConditionersList;
+
+/* return (
    <div className="conditioners container">
       <h2 className="text-center font-bold text-6xl text-slate-600">{brands}</h2>
       <ul ref={ref} className="conditioners__list grid grid-cols-3 gap-12 mt-14">
@@ -54,6 +81,4 @@ function ConditionersList({brands, data}: ConditionersProps) {
          })}
       </ul>
    </div>
-   )
-}
-export default ConditionersList;
+   ) */
--- app/air-conditioners/page.tsx
+++ app/air-conditioners/page.tsx
@@ -2,6 +2,26 @@ import {mideaConditionersData} from '../Data/Conditioners.data';
 import {welkinConditionersData} from '../Data/Conditioners.data';
 import ConditionersList from './index';
 
+import ultraviolet from '../../public/img/Equip/AirConditioners/Midea/Ultraviolet.jpg';
+import naomi from '../../public/img/Equip/AirConditioners/Midea/Naomi.png';
+import lotusS from '../../public/img/Equip/AirConditioners/Midea/Lotus-gold.png';
+import lotusG from '../../public/img/Equip/AirConditioners/Midea/Lotus-gold.png';
+import gaia from '../../public/img/Equip/AirConditioners/Midea/Gaia.png';
+import breezeless from '../../public/img/Equip/AirConditioners/Midea/Brezeless.png';
+import alba from '../../public/img/Equip/AirConditioners/Midea/Alba.png';
+import aep from '../../public/img/Equip/AirConditioners/Midea/AEP.png';
+
+import apollon from '../../public/img/Equip/AirConditioners/Welkin/Apollon.jpg';
+import ataman from '../../public/img/Equip/AirConditioners/Welkin/Ataman.jpg';
+import epic from '../../public/img/Equip/AirConditioners/Welkin/Epic.png';
+import epicG from '../../public/img/Equip/AirConditioners/Welkin/Epic-Gold.png';
+import general from '../../public/img/Equip/AirConditioners/Welkin/General.jpg';
+import novus from '../../public/img/Equip/AirConditioners/Welkin/Novus.jpg';
+import nuar from '../../public/img/Equip/AirConditioners/Welkin/Nuar.png';
+import sirius from '../../public/img/Equip/AirConditioners/Welkin/Sirius.jpg';
+import vavilon from '../../public/img/Equip/AirConditioners/Welkin/Vavilon.jpg';
+import zizoo from '../../public/img/Equip/AirConditioners/Welkin/Zizoo.jpg';
+
 import Conditioners from "./index";
 const brands = ['Кондиционеры Midea', 'Кондиционеры Welkin'];
 
@@ -12,13 +32,19 @@ async function getConditionsData() {
    }
    return res.json();
 }
-
+const imgesMidea = [ultraviolet, naomi, lotusS, lotusG, gaia, breezeless, alba, aep];
+const imgesWelkin = [apollon, ataman, epic, epicG, general, novus, nuar, sirius, vavilon, zizoo]
 async function AirConditiones() {
    const data = await getConditionsData();
    console.log(data)
    return (
    <section className='flex-auto'>
-      <ConditionersList brands={brands} data={data} />
+      {brands.map((el, index) => {
+         return (
+            <ConditionersList brand={el} data={index === 0 ? data.midea : data.welkin} imges={index === 0 ? imgesMidea : imgesWelkin} />
+         )
+      })}
+      
    </section>
    )
 }
