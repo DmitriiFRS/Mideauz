e633198e439e777e33cf233aed161df26614dab3
Дмитрий <Dmitriifrs@gmail.com>
2023-11-08 17:45:29 +0500

    continue cart design

--- app/cart/Main.tsx
+++ app/cart/Main.tsx
@@ -15,29 +15,31 @@ function Main({ value }: { value: Array<CartItemType> }) {
    }, [value]);
    return (
       clientValue && (
-         <div className="cart__body grid grid-cols-2">
-            <div className="cart__main">
-               {!value.length ? (
-                  <h2>Корзина пуста</h2>
-               ) : (
-                  <ul className="">
-                     {value.map((el: CartItemType, index: number) => {
-                        return (
-                           <li key={index} className="cart__item mt-8">
-                              <Link href={"/"}>
-                                 <p>
-                                    Модель: {el.model} {el.power} BTU
-                                 </p>
-                                 <p>Количество: {el.count}</p>
-                                 <p>Стоимость: {el.price * el.count} UZS</p>
-                              </Link>
-                           </li>
-                        );
-                     })}
-                  </ul>
-               )}
+         <div className="cart__body mt-10 mx-6">
+            <div className={`cart__bodyContainer grid ${!value.length ? "cart__bodyContainer__wfull" : ""}`}>
+               <div className="container cart__main">
+                  {!value.length ? (
+                     <h2 className="text-center">Корзина пуста</h2>
+                  ) : (
+                     <ul className="">
+                        {value.map((el: CartItemType, index: number) => {
+                           return (
+                              <li key={index} className="cart__item mt-8">
+                                 <Link href={"/"}>
+                                    <p>
+                                       Модель: {el.model} {el.power} BTU
+                                    </p>
+                                    <p>Количество: {el.count}</p>
+                                    <p>Стоимость: {el.price * el.count} UZS</p>
+                                 </Link>
+                              </li>
+                           );
+                        })}
+                     </ul>
+                  )}
+               </div>
+               <div className={`cart__additional ${!value.length ? "cart__additional__disable" : ""}`}>fd</div>
             </div>
-            <div className="cart__additional">fd</div>
          </div>
       )
    );
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -11,9 +11,10 @@ function Cart() {
    }
    return (
       <div className="cart mt-14 flex-auto">
-         <div className="cart__header">
-            <h2 className="inline-block w-full text-center text-4xl">Корзина товаров</h2>
-            <button className="cart__clear mt-10" onClick={clearCart}>
+         <div className="cart__header container flex items-center">
+            <h2 className="text-4xl flex-auto">Корзина товаров</h2>
+            <button className="cart__clear">Вернуться к покупкам</button>
+            <button className="cart__clear ml-3" onClick={clearCart}>
                Очистить корзину
             </button>
          </div>
