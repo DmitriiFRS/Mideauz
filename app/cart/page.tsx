4ae8b8410bb0aaad53764b044378be3424fc57c6
Дмитрий <Dmitriifrs@gmail.com>
2023-11-13 16:52:36 +0500

    prepare to reusable modal

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,10 +4,12 @@ import { useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Main from "./Main";
 import "./cart.scss";
+import { useRouter } from "next/navigation";
 
 function Cart() {
    const [value, setValue] = useLocalStorage("item", []);
    const [isModalOpen, setModalStatus] = useState<boolean>(false);
+   const router = useRouter();
    function clearCart() {
       setValue([]);
       setModalStatus(false);
@@ -16,14 +18,17 @@ function Cart() {
       if (!value.length) return false;
       setModalStatus(true);
    }
+   function back() {
+      router.back();
+   }
    return (
       <div className="cart mt-14 flex-auto">
          <div className="cart__header container flex items-center">
             <h2 className="text-4xl font-medium flex-auto">Корзина товаров</h2>
-            <button className="cart__btnBody ml-3 flex items-center justify-center">
-               <span className="cart__btn relative z-10">Вернуться к покупкам</span>
+            <button onClick={back} className="cart__btnBody ml-3 flex items-center justify-center">
+               <span className="cart__btn relative z-10">Вернуться назад</span>
             </button>
-            <button className="cart__btnBody ml-3 flex items-center justify-center" onClick={clearModalOpen}>
+            <button onClick={clearModalOpen} className="cart__btnBody ml-3 flex items-center justify-center">
                <span className="cart__btn relative z-10">Очистить корзину</span>
             </button>
          </div>
