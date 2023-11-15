f92911040da86017f614b0559f7e6fcb43a5008d
Дмитрий <Dmitriifrs@gmail.com>
2023-11-15 15:29:05 +0500

    added video to mainpage

--- app/Main/Preview/Preview.tsx
+++ app/Main/Preview/Preview.tsx
@@ -1,22 +1,26 @@
-import './Preview.scss';
-import Image from 'next/image';
-import bg from '../../../public/img/Preview-bg.jpeg'
+import "./Preview.scss";
+import Image from "next/image";
+import bg from "../../../public/img/Preview-bg.jpeg";
 
 function Preview() {
-return (
-   <section className="py-56 w-full h-full">
-      <div className="preview__bg absolute left-0 top-0 w-full object-cover z-0">
-         <Image className='object-cover object-center' src={bg} alt='bg' fill={true} />
-      </div>
-      <div className="container flex flex-col items-center justify-center h-full relative z-10 text-white">
-         <h1 className="preview__title text-center">Страница официального представителя Midea в Узбекистане</h1>
-         <h2 className='preview__subtitle text-4xl font-medium mt-11 text-center'>По всем вопросам обращайтесь к нашим специалистам</h2>
-         <div className="preview__phones-container flex justify-around text-3xl italic">
-            <div className="preview__phones font-semibold">+998 71 200 05 45</div>
-            <div className="preview__phones font-semibold">+998 99 443 06 66</div>
+   return (
+      <section className="py-56 w-full h-full">
+         <div className="preview__bg absolute left-0 top-0  object-cover z-0">
+            <video loop autoPlay muted>
+               <source src="/video/main.mp4" />
+            </video>
          </div>
-      </div>
-   </section>
-)
+         <div className="container flex flex-col items-center justify-center h-full relative z-10 text-white">
+            <h1 className="preview__title text-center">Страница официального представителя Midea в Узбекистане</h1>
+            <h2 className="preview__subtitle text-4xl font-medium mt-11 text-center">
+               По всем вопросам обращайтесь к нашим специалистам
+            </h2>
+            <div className="preview__phones-container flex justify-around text-3xl italic">
+               <div className="preview__phones font-semibold">+998 71 200 05 45</div>
+               <div className="preview__phones font-semibold">+998 99 443 06 66</div>
+            </div>
+         </div>
+      </section>
+   );
 }
 export default Preview;
