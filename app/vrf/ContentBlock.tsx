9df76d6e9a7583c9af18f760b506b84bf9909e0e
Дмитрий <Dmitriifrs@gmail.com>
2023-12-05 15:52:41 +0500

    anim

--- app/vrf/ContentBlock.tsx
+++ app/vrf/ContentBlock.tsx
@@ -1,7 +1,8 @@
+import ContentBlockClient from "./ContentBlockClient";
 import "./vrf.scss";
 import Image from "next/image";
 
-type ContentBlockProps = {
+export type ContentBlockProps = {
    title: string;
    list: Array<string>;
    img: any;
@@ -11,20 +12,7 @@ function ContentBlock({ content }: { content: ContentBlockProps }) {
    return (
       <div className="vrf-content mt-20">
          <h4 className="text-center font-semibold text-4xl text-slate-500">{content.title}</h4>
-         <div className="mt-20 flex justify-around">
-            <ul className="vrf-content__list flex flex-col justify-center">
-               {content.list.map((el, index) => {
-                  return (
-                     <li key={index} className="text-lg vrf-content__item text-slate-600">
-                        {el}
-                     </li>
-                  );
-               })}
-            </ul>
-            <div className="vrf-content__imgBody relative">
-               <Image src={content.img} alt="vrf" fill />
-            </div>
-         </div>
+         <ContentBlockClient content={content} />
       </div>
    );
 }
---
+++ app/vrf/ContentBlockClient.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { ContentBlockProps } from "./ContentBlock";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+
+function ContentBlockClient({ content }: { content: ContentBlockProps }) {
+   const [isInView, setIsView] = useState<boolean>(false);
+   const { ref, inView } = useInView({
+      threshold: 0.3,
+      triggerOnce: true,
+   });
+   useEffect(() => {
+      setIsView(inView);
+   }, [inView]);
+   return (
+      <div
+         ref={ref}
+         className={`vrf-content__body mt-20 flex justify-around ${isInView ? "vrf-content__itemActive" : ""}`}
+      >
+         <ul className="vrf-content__list flex flex-col justify-center">
+            {content.list.map((el, index) => {
+               return (
+                  <li key={index} className="text-lg vrf-content__item text-slate-600">
+                     {el}
+                  </li>
+               );
+            })}
+         </ul>
+         <div className="vrf-content__imgBody relative">
+            <Image src={content.img} alt="vrf" fill />
+         </div>
+      </div>
+   );
+}
+export default ContentBlockClient;
