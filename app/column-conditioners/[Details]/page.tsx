793c51888fe1e63a352fe549a4971ca338b79100
Дмитрий <Dmitriifrs@gmail.com>
2023-12-12 15:42:38 +0500

    сделал колонники

--- app/column-conditioners/[Details]/page.tsx
+++ app/column-conditioners/[Details]/page.tsx
@@ -2,14 +2,9 @@
 
 import { useDispatch, useSelector } from "react-redux";
 import "../column-conditioners.scss";
-import { useParams } from "next/navigation";
 import { AppDispatch, RootState } from "@/app/Redux/store";
-import { currencyValueData } from "@/app/Redux/Slices/main.slice";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { getConditionerItems } from "@/app/Redux/Slices/items.slice";
-import Image from "next/image";
-import Video from "./Video";
-import SelectPowerInput from "./SelectPowerInput";
 import ItemListpage from "@/app/ReusableComponents/ItemsList/ItemListpage";
 
 type ModelsType = {
@@ -35,7 +30,7 @@ function Details() {
    const goods = useSelector((state: RootState) => state.itemReducer.itemsList);
    useEffect(() => {
       dispatch(getConditionerItems());
-   }, [dispatch]);
+   }, []);
    return goods?.колонники ? <ItemListpage items={goods.колонники} /> : "first null";
 }
 export default Details;
