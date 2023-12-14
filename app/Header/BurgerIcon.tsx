44a13b81b57e4ac2e12cbc6042d99fcd85384ece
Дмитрий <Dmitriifrs@gmail.com>
2023-12-14 16:27:08 +0500

    done burger

--- app/Header/BurgerIcon.tsx
+++ app/Header/BurgerIcon.tsx
@@ -1,6 +1,10 @@
 function BurgerIcon({ setMenuOpen }: { setMenuOpen: (e: boolean) => void }) {
+   function openMenu() {
+      setMenuOpen(true);
+      document.body.style.overflow = "hidden";
+   }
    return (
-      <button onClick={() => setMenuOpen(true)} className="header__burgerBtn">
+      <button onClick={openMenu} className="header__burgerBtn">
          <span></span>
          <span></span>
          <span></span>
--- app/Header/BurgerNav.tsx
+++ app/Header/BurgerNav.tsx
@@ -1,30 +1,99 @@
-import { IoIosClose } from "react-icons/io";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 function BurgerNav({ isMenuOpen, setMenuOpen }: { isMenuOpen: boolean; setMenuOpen: (e: boolean) => void }) {
+   const [isSubMenuOpen, setSubMenuOpen] = useState(false);
+   function toggleMenu() {
+      setSubMenuOpen(true);
+   }
+   function closeMenu() {
+      setMenuOpen(false);
+      setSubMenuOpen(false);
+   }
+   useEffect(() => {
+      window.addEventListener("resize", closeMenu);
+      return () => {
+         window.removeEventListener("resize", closeMenu);
+      };
+   }, []);
    return (
       <div
          className={`header__burgerMenu ${
             isMenuOpen ? "header__burgerMenu__active" : ""
-         } absolute left-0 top-0 h-screen w-full bg-white`}
+         } absolute left-0 top-0 h-screen w-full bg-white overflow-auto`}
       >
          <div className="header__burgerBody relative w-full h-full">
-            <button onClick={() => setMenuOpen(false)} className="header__burgerCloseBody absolute right-0 top-0">
-               <IoIosClose style={{ width: "100%", height: "100%" }} />
+            <button onClick={closeMenu} className="header__burgerCloseBody absolute right-2 top-2">
+               <IoCloseSharp style={{ width: "100%", height: "100%" }} />
             </button>
-            <nav className="header__burgerNav">
-               <ul className="header__list">
-                  <li className="header__BurgerItem">1</li>
-                  <li className="header__BurgerItem">2</li>
-                  <li className="header__BurgerItem">3</li>
-                  <li className="header__BurgerItem">4</li>
+            <button
+               onClick={() => setSubMenuOpen(false)}
+               className={`header__burgerBack ${
+                  isSubMenuOpen ? "header__burgerBack__active" : ""
+               } absolute left-2 top-2`}
+            >
+               <IoIosArrowBack style={{ width: "100%", height: "100%" }} />
+            </button>
+            <nav className="header__burgerNav flex justify-center w-full h-full relative">
+               <ul
+                  className={`header__burgerList ${
+                     isSubMenuOpen ? "header__burgerList__hide" : ""
+                  } mt-36 absolute top-0 w-full flex flex-col items-center`}
+               >
+                  <li onClick={toggleMenu} className="header__BurgerItem">
+                     Оборудование
+                  </li>
+                  <Link onClick={closeMenu} className="header__burgerItem" href={"/coordinates"}>
+                     <li>Координаты</li>
+                  </Link>
+                  <Link onClick={closeMenu} className="header__burgerItem" href={"/about"}>
+                     <li>О компании</li>
+                  </Link>
+                  <Link onClick={closeMenu} className="header__burgerItem" href={"/catalog"}>
+                     <li>Каталоги</li>
+                  </Link>
                </ul>
-               <ul className="header__sublist">
-                  <li className="header__BurgerSubitem">1</li>
-                  <li className="header__BurgerSubitem">2</li>
-                  <li className="header__BurgerSubitem">3</li>
-                  <li className="header__BurgerSubitem">4</li>
-                  <li className="header__BurgerSubitem">5</li>
-                  <li className="header__BurgerSubitem">6</li>
+               <ul
+                  className={`header__sublist ${
+                     isSubMenuOpen ? "header__sublist__open" : ""
+                  } mt-36 absolute top-0 px-2 text-center`}
+               >
+                  <Link
+                     onClick={closeMenu}
+                     className="header__burgerSubitem header__burgerItem"
+                     href={"/air-conditioners"}
+                  >
+                     <li>Бытовые кондиционеры настенного типа</li>
+                  </Link>
+                  <Link
+                     onClick={closeMenu}
+                     className="header__burgerSubitem header__burgerItem"
+                     href={"/column-conditioners"}
+                  >
+                     <li>Бытовые кондиционеры колонного типа</li>
+                  </Link>
+                  <Link
+                     onClick={closeMenu}
+                     className="header__burgerSubitem header__burgerItem"
+                     href={"/casette-conditioners"}
+                  >
+                     <li>Полупромышленные кондиционеры кассетного типа</li>
+                  </Link>
+                  <Link
+                     onClick={closeMenu}
+                     className="header__burgerSubitem header__burgerItem"
+                     href={"/ducted-conditioners"}
+                  >
+                     <li>Полупромышленные кондиционеры канального типа</li>
+                  </Link>
+                  <Link onClick={closeMenu} className="header__burgerSubitem header__burgerItem" href={"/vrf"}>
+                     <li>VRF системы</li>
+                  </Link>
+                  <Link onClick={closeMenu} className="header__burgerSubitem header__burgerItem" href={"/chillers"}>
+                     <li>Чиллер-фанкойл Midea</li>
+                  </Link>
                </ul>
             </nav>
          </div>
