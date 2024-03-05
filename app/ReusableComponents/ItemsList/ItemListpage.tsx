42f473007e686e299c4fa66a57b7f3833a7b9bf4
Дмитрий <Dmitriifrs@gmail.com>
2024-03-05 12:07:41 +0500

    colDesign

--- app/ReusableComponents/ItemsList/ItemListpage.tsx
+++ app/ReusableComponents/ItemsList/ItemListpage.tsx
@@ -38,24 +38,29 @@ function ItemListpage({
                return (
                   <div key={index} className="colConditionerCard container flex flex-col flex-auto">
                      <div className="colConditionerCard__body grid">
-                        <div className="colConditionerCard__imgBody relative">
-                           <Image src={el.col.image.node.sourceUrl} alt={el.col.name} fill></Image>
+                        <h2 className="colConditionerCard__title text-4xl font-semibold text-center">
+                           Колонный кондиционер {el.col.name} {el.col.company}
+                        </h2>
+                        <div className="colConditionerCard__mainBody flex justify-between">
+                           <div className="colConditionerCard__imgBody relative mt-14">
+                              <Image src={el.col.image.node.sourceUrl} alt={el.col.name} fill></Image>
+                           </div>
+                           <div className="colConditionerCard__select flex flex-col mt-14">
+                              <SelectPowerInput
+                                 model={el}
+                                 currencyValue={currencyValue}
+                                 items={items}
+                                 hrefName={hrefName}
+                              />
+                           </div>
                         </div>
-                        <div className="colConditionerCard__select flex flex-col">
-                           <p className="text-4xl font-semibold">{el.col.name}</p>
-                           <SelectPowerInput
-                              model={el}
-                              currencyValue={currencyValue}
-                              items={items}
-                              hrefName={hrefName}
-                           />
-                        </div>
-                        <div className="colConditionerCard__descBody flex flex-col mt-14">
-                           <div className="text-slate-600">
+
+                        <div className="colConditionerCard__descBody flex flex-col mt-20">
+                           <div className="text-slate-600 max-w-6xl">
                               <h4 className="text-center text-2xl font-semibold">Описание</h4>
                               <p className="colConditionerCard__desc mt-10">{el.col.description}</p>
                            </div>
-                           <div className="colConditionerCard__paramBody text-slate-600 w-full mt-14 py-6">
+                           <div className="colConditionerCard__paramBody text-slate-600 w-full mt-20 py-6">
                               <h4 className="text-2xl font-semibold text-center">Основные параметры</h4>
                               <ul className="colConditionerCard__advantages mt-10 grid w-full">
                                  <li className="colConditionerCard__list-disc">
