814c2ca1e73595feb7c81e710ee5a4a4fc978c8f
Дмитрий <Dmitriifrs@gmail.com>
2024-03-11 13:30:55 +0500

    перенаправил на beget wp

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
       remotePatterns: [
          {
             protocol: "http",
-            hostname: "wordpress",
+            hostname: "dmitrpjh.beget.tech",
          },
       ],
    },
