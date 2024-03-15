d3259b97399600a69ad77459f572f5ad7f4abff2
Дмитрий <Dmitriifrs@gmail.com>
2024-03-15 16:41:09 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
+      unoptimized: true,
       remotePatterns: [
          {
             protocol: "http",
             hostname: "dmitrpjh.beget.tech",
          },
+         {
+            protocol: "https",
+            hostname: "midea-uzb.uz",
+         },
       ],
    },
+   output: "export",
 };
 
 module.exports = nextConfig;
